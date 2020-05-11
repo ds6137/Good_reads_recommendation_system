47d70d1705de66d648c60eb8e29ed44380a83e40
Devansh Singh <devansh.singh@a-10-27-21-139.dynapool.vpn.nyu.edu>
2020-05-11 15:44:05 -0400

    final training script without hyper parameter tuning

--- training_normal.py
+++ training_normal.py
@@ -25,25 +25,25 @@ def main(spark, data_file, model_file):
 
     #training_data = 'hdfs:/user/ds6137/train_data_final.parquet'
     train_data = spark.read.parquet(data_file)
-
+    print("read the training data")
     #train_data = spark.read.parquet('train_data_final.parquet')
     
     train_data.createOrReplaceTempView('train_data')
 
-    als = ALS(maxIter=10, regParam = 0.01, rank = 10, \
-                          userCol="user_id", itemCol="book_id", ratingCol="rating",\
-                          coldStartStrategy="drop",implicitPrefs=True)
+    als = ALS(maxIter=10, regParam = 0.1, rank = 100, userCol="user_id", itemCol="book_id", ratingCol="rating", coldStartStrategy="drop",implicitPrefs=True)
+    # fitting the training data to the ALS model
     als_model_normal = als.fit(train_data)
-
+    # saving the trained model
     als_model_normal.save(model_file)
-
+    print("Model fitted and saved")
 
 if __name__ == "__main__":
     print("Starting main")
     # Set parameters and create the spark session object
-    spark =SparkSession.builder.appName("training_normal").config("spark.executor.memory", "32g").config("spark.driver.memory", "32g").config("spark.driver.maxResultsSize", "30G").getOrCreate()
+    spark = SparkSession.builder.appName("training_normal").master('yarn').config("spark.executor.memory", "15g").config("spark.driver.memory", "15g").config("spark.driver.maxResultsSize", "30G").getOrCreate()
+    # spark = SparkSession.builder.appName("training_normal").getOrCreate()
     print("Spark session created")
-
+    # training data file from command line
     data_file = sys.argv[1]
 
     # Get the model filename from the command line
